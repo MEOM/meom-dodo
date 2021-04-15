0532a967bbdde1b1ef260bbd46bc1f2b44f1e3ac
Sami Keijonen <sami.keijonen@foxnet.fi>
2021-04-15 14:11:06 +0300

    Initial commit.

---
+++ meom-dodo.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin name: MEOM Dodo
+ * Author: MEOM
+ * Author URI: https://www.meom.fi/
+ * Description: Clean up WordPress.
+ * Version: 1.0
+ */
+
+// Exit if accessed directly.
+if ( ! defined( 'ABSPATH' ) ) {
+    exit;
+}
