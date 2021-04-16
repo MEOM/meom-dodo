393f83f3486d97fb90ad0cc8793c88629ae90bf6
Sami Keijonen <sami.keijonen@foxnet.fi>
2021-04-16 15:44:48 +0300

    Add language files. They don't work yet.

--- meom-dodo.php
+++ meom-dodo.php
@@ -4,6 +4,9 @@
  * Author: MEOM
  * Author URI: https://www.meom.fi/
  * Description: Clean up WordPress.
+ * License: GPL2 or later.
+ * Text Domain: meom-dodo
+ * Domain Path: /languages
  * Version: 1.0.2
  */
 
@@ -11,6 +14,12 @@
 if ( ! defined( 'ABSPATH' ) ) {
     exit;
 }
+
+// Load text domain.
+function meom_dodo_i18n() {
+    load_plugin_textdomain( 'meom-dodo', false, trailingslashit( dirname( plugin_basename( __FILE__ ) ) ) . 'languages' );
+}
+add_action( 'plugins_loaded', 'meom_dodo_i18n', 2 );
 
 require_once dirname( __FILE__ ) . '/inc/contact-page.php';
 require_once dirname( __FILE__ ) . '/inc/enqueue-assets.php';
