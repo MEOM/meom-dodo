3614f8552aca53e31437650e5a30c2d78a0f376d
Sami Keijonen <sami.keijonen@foxnet.fi>
2021-04-30 09:08:26 +0300

    Remove extra contact info link for now.

--- inc/contact-dashboard-widget.php
+++ inc/contact-dashboard-widget.php
@@ -25,7 +25,7 @@ add_action( 'wp_dashboard_setup', __NAMESPACE__ . '\add_dashboard_contact_widget
 function contact_content() {
     ?>
     <p>Mikäli kohtaat sivustolla ongelmia, voit ottaa yhteyttä MEOMin tukeen sähköpostilla <a href="mailto:support@meom.fi">support@meom.fi</a>.</p> 
-    <p>Kerrothan viestissäsi tarkan kuvauksen ongelmasta ja sen sijainnista sekä käyttämäsi selaimen ja laitteen. Katso lisää ylläpidostamme täältä LINKKI.</p>
+    <p>Kerrothan viestissäsi tarkan kuvauksen ongelmasta ja sen sijainnista sekä käyttämäsi selaimen ja laitteen.</p>
     <P>Mikäli tarvitset apua sisällönsyöttöön, markkinointiin, SEO-asioihin tai muuhun vastaavaan, voit ottaa yhteyttä samaan osoitteeseen <a href="mailto:support@meom.fi">support@meom.fi</a>.</p>
     <p>Mikäli haluat sivustolle lisää ominaisuuksia, uutta ilmettä tai mitä vain lisäkehitystä, niin ota yhteyttä <a href="mailto:sales@meom.fi">sales@meom.fi</a>.</p>
     <?php
