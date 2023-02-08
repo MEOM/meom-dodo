5348e237583e2f4c1069f561af81259f52333df4
Sami Keijonen <sami.keijonen@foxnet.fi>
2023-02-08 15:47:48 +0200

    Fix comment.

--- inc/remove-svg-filters.php
+++ inc/remove-svg-filters.php
@@ -13,7 +13,7 @@ namespace MEOM\Dodo;
  * @return void
  */
 function remove_svg_filters() {
-    // Allow filtering layout support.
+    // Allow filtering svg filters.
     if ( \apply_filters( 'meom_dodo_remove_svg_filters', true ) ) {
         remove_action( 'wp_body_open', 'wp_global_styles_render_svg_filters' );
     }
