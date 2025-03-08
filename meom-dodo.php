d70a259ec871ca3a0d3b02ddf170be277b5ccc55
Sami Keijonen <sami.keijonen@foxnet.fi>
2025-03-08 17:56:38 +0200

    Set Seravo security settings on by default

--- meom-dodo.php
+++ meom-dodo.php
@@ -21,9 +21,10 @@ function meom_dodo_i18n() {
 }
 add_action( 'plugins_loaded', 'meom_dodo_i18n', 2 );
 
-require_once dirname( __FILE__ ) . '/inc/contact-dashboard-widget.php';
-require_once dirname( __FILE__ ) . '/inc/enqueue-assets.php';
-require_once dirname( __FILE__ ) . '/inc/remove-admin-menu-items.php';
-require_once dirname( __FILE__ ) . '/inc/remove-editor-settings.php';
-require_once dirname( __FILE__ ) . '/inc/remove-svg-filters.php';
-require_once dirname( __FILE__ ) . '/inc/user-meta.php';
+require_once __DIR__ . '/inc/contact-dashboard-widget.php';
+require_once __DIR__ . '/inc/enqueue-assets.php';
+require_once __DIR__ . '/inc/remove-admin-menu-items.php';
+require_once __DIR__ . '/inc/remove-editor-settings.php';
+require_once __DIR__ . '/inc/remove-svg-filters.php';
+require_once __DIR__ . '/inc/security.php';
+require_once __DIR__ . '/inc/user-meta.php';
